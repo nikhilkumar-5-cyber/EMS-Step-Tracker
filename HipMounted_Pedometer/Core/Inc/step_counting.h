303477d463f030404aa3134ac419802e2ce8937a
n04h24 <noahnourdine8@gmail.com>
2026-05-20 09:20:21 +1000

    Integrating peakSeries && REF_SAMPLES

--- HipMounted_Pedometer/Core/Inc/step_counting.h
+++ HipMounted_Pedometer/Core/Inc/step_counting.h
@@ -12,10 +12,13 @@
 #include "magnitude.h"
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
 /* Defines */
 #define PEAK_THRESHOLD 0.6 //acceleration [m/s^2]
-#define PEAK_SERIES_SIZE 20 //~1x step-size MAX
+#define PEAK_SERIES_SIZE 18 //~1x step-size MAX
+#define PEAK_SAMPLES 10
+#define REF_SAMPLES 2
 #define MAX_MOVING_TIME 300 //50% of AVG_STEP [ms]
 #define MIN_PEAK_TIME 200
 #define ISO_SAMPLES 3 //Recent (3) samples
@@ -25,9 +28,10 @@
 extern uint8_t VECTOR_STATE;
 
 /* Function prototypes */
+bool bothAbove(double a, double b, double threshold);
 
-void update_lastSamples(ADXL335_t *dest);
+void updateLastSamples(ADXL335_t *dest);
 
-void gait_cycle(); //Control step-tracking states
+void gaitCycle(); //Control step-tracking states
 
 #endif /* INC_STEP_COUNTING_H_ */
--- HipMounted_Pedometer/Core/Src/step_counting.c
+++ HipMounted_Pedometer/Core/Src/step_counting.c
@@ -7,30 +7,39 @@
 
 #include "step_counting.h"
 
-/* Global Variables */
+/* Global variables */
+uint8_t vectorState = 0;
 
-uint8_t vectorState = 0; //EXTERN
+/* Local variables */
+uint8_t referenceSamples = 0;
 STEP_CLOCK_t timeCard = {0};
 ADXL335_t startVector = {0};
+ADXL335_t maxPeakVector = {0};
+ADXL335_t peakSeries[PEAK_SAMPLES] ={0};
+uint8_t peakSeriesIndex = 0;
 ADXL335_t lastSamples[ISO_SAMPLES] = {0}; //Local processing buffer
 
-/* INTERRUPT (Sample Acceleration >> Receive Magnitude) */
+bool bothAbove(double a, double b, double threshold) {
 
-void update_lastSamples(ADXL335_t *dest) {
+	return a > threshold && b > threshold;
+
+}
+
+void updateLastSamples(ADXL335_t *dest) {
 
 	for (uint8_t i = 0; i < ISO_SAMPLES; i++) {
 		lastSamples[i] = SAMPLE_BUFFER[i]; //Update first (ISO_SMAPLES)
 	}
-
+	//FIX: Subject to race-conditions
 }
 
-void gait_cycle() { //NOTE: call for every NEW sample
+void gaitCycle() { //NOTE: call for every NEW sample
 
 	switch (vectorState) {
 
 	case (0): //IDLE
 
-		update_lastSamples(lastSamples); //Update local processing buffer
+		updateLastSamples(lastSamples); //Update local processing buffer
 		uint8_t decreasing = 1; //Flag indicating: 0th > 1st > 2nd...
 
 		for (uint8_t i = 0; i < ISO_SAMPLES-1; i++) {
@@ -53,24 +62,42 @@ void gait_cycle() { //NOTE: call for every NEW sample
 	/* MOVING */
 	case (1):
 
-		update_lastSamples(lastSamples); //Update local processing buffer
+		updateLastSamples(lastSamples); //Update local processing buffer
 
 		if (HAL_GetTick() - timeCard.begin >= MAX_MOVING_TIME) {
 			vectorState = 0; //Return to IDLE
 		}
-		else if (lastSamples[0].magnitude > PEAK_THRESHOLD && lastSamples[1].magnitude > PEAK_THRESHOLD) { //2x Samples > Threshold
-			vectorState = 2; //Progress to STEPPING
+		else { //2x Samples > Threshold
+			for (uint8_t i = 0; i < REF_SAMPLES-1; i++) {
+				if (lastSamples[i].magnitude > lastSamples[i+1].magnitude && bothAbove(lastSamples[i].magnitude, lastSamples[i+1].magnitude, PEAK_THRESHOLD)) {
+					//Flag or other condition
+				}
+			}
+
+			//FIX: Change to REF_SAMPLES for loop
+			vectorState = 2; //Progress to PEAKING
 			timeCard.peak_start = HAL_GetTick();
+
+			//Add previous samples for reference
+			peakSeriesIndex = 0;
+			memset(peakSeries, 0, sizeof(peakSeries)); //Clear buffer
+			for (; peakSeriesIndex < REF_SAMPLES; peakSeriesIndex++) {
+				peakSeries[peakSeriesIndex] = lastSamples[peakSeriesIndex];
+			}
 		}
 
 		break; /* CHANGE STATE: Timeout >> (0); OR Break threshold >> (2) */
 
-	/* STEPPING */
+	/* PEAKING */
 	case (2):
 
-		update_lastSamples(lastSamples); //Update local processing buffer
+		//Dynamically copy SAMPLE_BUFF entries into peakSamples
 
-		if (timeCard.peak_start >= MIN_PEAK_TIME && lastSamples[0].magnitude <= PEAK_THRESHOLD && lastSamples[1].magnitude <= PEAK_THRESHOLD){
+		//Update maxPeakVector
+
+		//Detect falling condition && below threshold
+
+		if (HAL_GetTick() - timeCard.peak_start >= MIN_PEAK_TIME && lastSamples[0].magnitude <= PEAK_THRESHOLD && lastSamples[1].magnitude <= PEAK_THRESHOLD){
 			vectorState = 0;
 			timeCard.end = HAL_GetTick();
 			timeCard.time = timeCard.end - timeCard.begin;
