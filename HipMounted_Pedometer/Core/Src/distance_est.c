21d49954edc82f52d9b1dd2b60b0a01d4257592b
n04h24 <noahnourdine8@gmail.com>
2026-06-02 20:18:53 +1000

    distanceTravelled Simplified

--- HipMounted_Pedometer/Core/Src/distance_est.c
+++ HipMounted_Pedometer/Core/Src/distance_est.c
@@ -18,25 +18,12 @@ double avgStepSize = 0;
 
 void addDistance(ADXL335_t *peakVector, ADXL335_t *startVector, ADXL335_t *endVector) {
 
-	double stepSize = fabs(endVector->magnitude - startVector->magnitude);
-
-	avgStepSize += STEP_SIZE_ALPHA * (stepSize - avgStepSize); //Exponential Moving Average
-
-	double alpha = 0; //Weighting * Peak(i)
-
-	if (stepSize > avgStepSize) {
-
-		alpha = 1.5;
-	}
-	else if (fabs(stepSize - avgStepSize) <= 0.1) {
-
-		alpha = 1.0;
-	}
-	else {
-
-		alpha = 0.5;
+	if (pace == WALKING) {
+		distanceTravelled += 0.8;
 	}
 
-	distanceTravelled += peakVector->magnitude * alpha;
+	else if (pace == RUNNING) {
+		distanceTravelled += 1.2;
+	}
 
 }
